7e282e0dcfb800846852f05588fe57449723a733
opurbo-b <bopurbo1302@gmail.com>
2024-10-09 22:38:27 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// Function that allwos the button to reveal or hide information
+function myFunction() {
+    var x = document.getElementById("main");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
